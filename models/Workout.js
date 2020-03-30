37450a00c3e25d3dd64c147278d1fb1fb3019d12
Jim Greasley <azjimgreasley@gmail.com>
2020-03-30 09:22:24 -0700

    Revise field name in schema Workout and delete unused schema Exercise

--- models/Workout.js
+++ models/Workout.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  date: {
-    type: Date,
-    default: Date.now
+  day: {
+        type: Date,
+        default: Date.now
         },
   exercises: [
     {
